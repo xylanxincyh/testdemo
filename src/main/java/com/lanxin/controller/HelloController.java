1a94e24564c764bc474efbfcf9453548d18bac7a
xylanxincyh <cyh_it@163.com>
2019-01-21 15:07:36 +0800

    ddd
    asdfsadfsafsad

--- src/main/java/com/lanxin/controller/HelloController.java
+++ src/main/java/com/lanxin/controller/HelloController.java
@@ -14,7 +14,7 @@ public class HelloController {
 
     @RequestMapping(value = "hello")
     public String hello(){
-        return "helloddddd";
+        return "helloddddddddddddd";
     }
 
     @RequestMapping(value = "empinfo")
